1edb0868a49315cc328e694deee59bb242c8645d
Robin <robindev2019@outlook.com>
2026-08-23 12:31:02 +0200

    Initial commit

---
+++ lib/novsh.ex
@@ -0,0 +1,18 @@
+defmodule Novsh do
+  @moduledoc """
+  Documentation for `Novsh`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Novsh.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/novsh_test.exs
@@ -0,0 +1,8 @@
+defmodule NovshTest do
+  use ExUnit.Case
+  doctest Novsh
+
+  test "greets the world" do
+    assert Novsh.hello() == :world
+  end
+end
